6da753162309e110c17577d3346299f20437372e
lyellly <1026439465@qq.com>
2023-03-08 17:45:43 +0800

    1

--- src/main/java/com/lyel/user/UserApplication.java
+++ src/main/java/com/lyel/user/UserApplication.java
@@ -1,13 +1,13 @@
-package com.example.mydemo;
+package com.lyel.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MydemoApplication {
+public class UserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MydemoApplication.class, args);
+        SpringApplication.run(UserApplication.class, args);
     }
 
 }
--- src/test/java/com/lyel/user/UserApplicationTests.java
+++ src/test/java/com/lyel/user/UserApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.mydemo;
+package com.lyel.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MydemoApplicationTests {
+class UserApplicationTests {
 
     @Test
     void contextLoads() {
